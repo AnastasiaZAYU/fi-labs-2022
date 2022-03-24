f4374af37c5cf91a17905558e539828bc640e312
AnastasiaZAYU <anastasiya7.0312@gmail.com>
2022-03-24 16:08:16 +0200

    Updated project cp1

--- cp_1/Zatsarenko_fi-94_Futurska_fi-94_cp1/Program.cs
+++ cp_1/Zatsarenko_fi-94_Futurska_fi-94_cp1/Program.cs
@@ -13,41 +13,54 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            string alph = "абвгдежзийклмнопрстуфхцчшщыьэюя ";
-            int n = alph.Length;
+            string alph = "абвгдежзийклмнопрстуфхцчшщыьэюя";
             FilterText();
-            var sr = new StreamReader("TEXT", Encoding.GetEncoding(866));
-            double[] s = Symbol(sr, alph);
-            for (int i = 0; i < n; i++)
-                Console.WriteLine("{0}  -  {1:N5}", alph[i], s[i]);
-            sr = new StreamReader("TEXT", Encoding.GetEncoding(866));
-            double[,] b = Biggram(sr, alph);
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (b[i,j] != 0)
-                        Console.WriteLine("{0}{1}  -  {2:N5}", alph[i], alph[j], b[i, j]);
-                }
-            }
-            sr = new StreamReader("TEXT", Encoding.GetEncoding(866));
-            double[,] b2 = BiggramIS(sr, alph);
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (b2[i, j] != 0)
-                        Console.WriteLine("{0}{1}  -  {2:N5}", alph[i], alph[j], b2[i, j]);
-                }
-            }
-            Console.WriteLine("H1 = {0:N5}", Entropy(s, n));
-            Console.WriteLine("H2 = {0:N5}", Entropy(b, n));
-            Console.WriteLine("H3 = {0:N5}", Entropy(b2, n));
-            Console.WriteLine("H1\\\" \" = {0:N5}", Entropy(s, n-1));
-            Console.WriteLine("H2\\\" \" = {0:N5}", Entropy(b, n - 1));
-            Console.WriteLine("H3\\\" \" = {0:N5}", Entropy(b2, n - 1));
+            var sr1 = File.OpenText("TEXT1");
+            double[] s1 = Symbol(sr1, alph + " ");
+            Print(s1, alph + " ");
+            var sr2 = File.OpenText("TEXT2");
+            double[] s2 = Symbol(sr2, alph);
+            Print(s2, alph);
+            sr1 = File.OpenText("TEXT1");
+            double[,] b1 = Biggram(sr1, alph + " ");
+            Print(b1, alph + " ");
+            sr2 = File.OpenText("TEXT2");
+            double[,] b2 = Biggram(sr2, alph);
+            Print(b2, alph);
+            sr1 = File.OpenText("TEXT1");
+            double[,] c1 = BiggramIS(sr1, alph + " ");
+            Print(c1, alph + " ");
+            sr2 = File.OpenText("TEXT2");
+            double[,] c2 = BiggramIS(sr2, alph);
+            Print(c2, alph);
+            Console.WriteLine("H1 = {0:N5}", Entropy(s1));
+            Console.WriteLine("H2 = {0:N5}", Entropy(b1));
+            Console.WriteLine("H2IS = {0:N5}", Entropy(c1));
+            Console.WriteLine("H1\\\" \" = {0:N5}", Entropy(s2));
+            Console.WriteLine("H2\\\" \" = {0:N5}", Entropy(b2));
+            Console.WriteLine("H2IS\\\" \" = {0:N5}", Entropy(c2));
+
             Console.ReadKey();
-            sr.Close();
+            sr1.Close();
+            sr2.Close();
+        }
+
+        static void Print(double[] array, string alph)
+        {
+            int n = alph.Length;
+            for (int i = 0; i < n; i++)
+                Console.WriteLine("{0}  -  {1:N5}", alph[i], array[i]);
+            Console.WriteLine();
+        }
+
+        static void Print(double[,] array, string alph)
+        {
+            int n = alph.Length;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)                
+                        if (array[i, j] != 0)                        
+                            Console.WriteLine("{0}{1}  -  {2:N5}", alph[i], alph[j], array[i, j]);            
+            Console.WriteLine();
         }
 
         static double[,] Biggram(StreamReader sr, string alph)
@@ -83,10 +96,8 @@ namespace Lab1
                 sum++;
             }
             for (i = 0; i < n; i++)
-            {
                 for (j = 0; j < n; j++)
                     count[i, j] = count[i, j] / sum;
-            }
             return count;
         }
 
@@ -99,94 +110,95 @@ namespace Lab1
             Array.Clear(count, 0, n);
             l2 = (char)sr.Read();
             for (j = 0; j < n; j++)
-            {
                 if (l2 == alph[j])
                     break;
-            }
             while (!sr.EndOfStream)
             {
                 l1 = l2;
                 i = j;
                 l2 = (char)sr.Read();
                 for (j = 0; j < n; j++)
-                {
                     if (l2 == alph[j])
                         break;
-                }
                 count[i, j]++;
                 sum++;
             }
             for (i = 0; i < n; i++)
-            {
                 for (j = 0; j < n; j++)
                     count[i, j] = count[i, j] / sum;
-            }
             return count;
         }
 
         static double[] Symbol(StreamReader sr, string alph)
         {
-            int k, sum = 0;
+            int sum = 0;
             char letter;
             int n = alph.Length;
             double[] count = new double[n];
             Array.Clear(count, 0, n);
             while (!sr.EndOfStream)
             {
-                k = sr.Read();
-                letter = (char)k;
+                letter = (char)sr.Read();
                 for (int i = 0; i < n; i++)
-                {
                     if (letter == alph[i])
                     {
                         count[i]++;
                         sum++;
                         break;
                     }
-                }
             }
             for (int i = 0; i < n; i++)
                 count[i] = count[i] / sum;
             return count;
         }
 
-        static double Entropy(double[,] count, int n)
+        static double Entropy(double[,] count)
         {
+            int n = count.GetLength(0);
             double h = 0;
-            for (int i = n - 1; i >= 0; i--) 
-            {
-                for (int j = n - 1; j >= 0; j--) 
-                {
+            for (int i = 0; i < n; i++) 
+                for (int j = 0; j < n; j++) 
                     if (count[i, j] != 0)
                         h += (count[i, j] * Math.Log(count[i, j], 2));
-                }
-            }
-            return -h;
+            return -h/2;
         }
 
-        static double Entropy(double[] count, int n)
+        static double Entropy(double[] count)
         {
             double h = 0;
-            for (int i = n - 1; i >= 0; i--) 
+            for (int i = 0; i < count.Length; i++)  
                 h += (count[i] * Math.Log(count[i], 2));
             return -h;
         }
 
         static void FilterText()
         {
-            var text = File.ReadAllText("TEXT", Encoding.GetEncoding(866));
+            var sr = File.OpenText("text.txt");
+            var sw1 = File.CreateText("TEXT1");
+            var sw2 = File.CreateText("TEXT2");
             int index;
-            text = text.ToLower();
-            for (int i = 0; i < text.Length; i++)
+            string text;
+            while (!sr.EndOfStream)
             {
-                index = (int)text[i];
-                if ((index < 1072 || index > 1103) && index != 32)
-                    text = text.Remove(i, 1).Insert(i, " ");
-                if (index == 1098)
-                    text = text.Remove(i, 1).Insert(i, "ь");
+                text = sr.ReadLine();
+                text = text.ToLower();
+                for (int i = 0; i < text.Length; i++)
+                {
+                    index = (int)text[i];
+                    if ((index < 1072 || index > 1103) && index != 32)
+                        text = text.Remove(i, 1).Insert(i, " ");
+                    if (index == 1098)
+                        text = text.Remove(i, 1).Insert(i, "ь");
+                }
+                text = Regex.Replace(text, @"\s+", " ");
+                text = text.TrimStart();
+                sw1.Write(text);
+                text = text.Replace(" ", string.Empty);
+                sw2.Write(text);
             }
-            text = Regex.Replace(text, @"\s+", " ");
-            File.WriteAllText("TEXT", text, Encoding.GetEncoding(866));
+            sw1.Close();
+            sw2.Close();
+            sr.Close();
         }
     }
 }
